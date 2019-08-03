c1e85ce429afce630c13fa8b47c0b71e0cbb0c62
shiv.prakash <shiv.prakash@cgi.com>
2019-08-03 09:42:04 +0530

    switch satatement demo commit

---
+++ src/main/java/com/stackroute/SwitchCaseDemo.java
@@ -0,0 +1,48 @@
+package com.stackroute;
+
+import java.util.Scanner;
+
+public class SwitchCaseDemo  {
+    public static void main(String[] args) {
+        System.out.println("enter character to check vowel or consonant");
+        Scanner scanner=new Scanner(System.in);
+       char ch= scanner.next().charAt(0);
+    
+        switch(ch)
+        {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            case 'A':
+                System.out.println("Vowel");
+                break;
+            case 'E':
+                System.out.println("Vowel");
+                break;
+            case 'I':
+                System.out.println("Vowel");
+                break;
+            case 'O':
+                System.out.println("Vowel");
+                break;
+            case 'U':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
+
