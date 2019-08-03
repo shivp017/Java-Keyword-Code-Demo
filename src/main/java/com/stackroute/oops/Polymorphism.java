4753f16c68bf536bbd2f9770386ef95dff8b1f12
shiv.prakash <shiv.prakash@cgi.com>
2019-08-03 10:43:02 +0530

    polymorphism

--- src/main/java/com/stackroute/oops/Polymorphism.java
+++ src/main/java/com/stackroute/oops/Polymorphism.java
@@ -5,10 +5,15 @@ public class Polymorphism {
     //method overloading
     static int add(int a, int b){return a+b;}
     static double add(double a, double b){return a+b;}
+    void run(){
+        System.out.println("Added result");
+    }
 }
-class TestOverloading2{
+class TestOverloading2 extends Polymorphism{
     public static void main(String[] args){
         System.out.println(Polymorphism.add(11,11));
         System.out.println(Polymorphism.add(12.3,12.6));
+        Polymorphism polymorphism=new Polymorphism();//method overidden
+        polymorphism.run();
     }}
 
